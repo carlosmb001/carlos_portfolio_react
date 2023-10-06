a587b0d09601be731cad21f95b59c5d3ac450058
Carlos Martinez Baltazar <42792565+carlosmb001@users.noreply.github.com>
2023-10-06 15:36:09 -0700

    added footer and header components

---
+++ src/components/Footer.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer>
+        <nav>
+            <ul>
+                <li><a href="https://www.linkedin.com/in/carlos-martinez-baltazar/"> <img src="./assests/images/icons/linkdin_icon.png" alt="linkdin icon"></a></li>
+                <li><a href="https://github.com/carlosmb001">
+                    <img src="./assests/images/icons/github_icon.png" alt="github icon"></a></li>
+                <li><a href="intagram.com">
+                    <img src="./assests/images/icons/insta_icon.png" alt="instagram icon">
+                </a></li>
+                <li><a href="twitter.com">
+                    <img src="./assests/images/icons/twitter_icon.png" alt="twitter icon">
+                </a></li>
+                <li>
+                    <a href="./assests/images/Final Resume Carlos Baltazar.pdf">
+                        <img src="./assests/images/icons/resume_icon.png" alt="resume icon">
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Header(props) {
+    return (
+        <header>
+        <section class="header">
+        <h1>Carlos Martinez</h1>
+        <nav>
+            <ul>
+                <li><a href="#about-me">About me</a></li>
+                <li><a href="#work">Work</a></li>
+                <li><a href="#contact-me">Contact me</a></li>
+                <li><a href="./assests/images/Final Resume Carlos Baltazar.pdf">Resume</a></li>
+            </ul>
+        </nav>
+        </section>
+    </header>
+    );
+}
+export default Header;
